34e8f176a7669bbc20b79da84ad0dede42870bb3
daniel <dh4487663@gmail.com>
2025-04-22 10:04:10 -0400

    seguro

--- core/models.py
+++ core/models.py
@@ -26,3 +26,33 @@ class Categoria(models.Model) :
     def __str__(self) :
         return self.nombre
 
+class Producto(models.Model) : 
+    proveedor = models.ForeignKey(Proveedor, on_delete= models.CASCADE, related_name = 'productos')
+    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE, related_name= 'productos')
+    nombre = models.CharField(max_length = 100)
+    slug = models.SlugField(max_length = 100, unique = True)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digit = 100, decimal_places = 2)
+    cantidad = models.PositiveIntegerField()
+    imagen = models.ImageField()
+    creacion = models.DateTimeField(auto_now_add = True)
+    actualizacion = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Orden(models.Model):
+    cliente = models.ForeignKey(Usuario, on_delete = models.CASCADE, related_name = 'orden')
+    productos = models.ManyToManyField(Producto, through= 'articulo de pedido')
+    precip_total = models.DecimalField(max_digits = 100, decimal_places = 2)
+    direccion_envio = models.FileField()
+    creacion = models.DateTimeField(auto_now_add = True)
+    actualizacion = models.DateTimeField(auto_now = True)
+
+class ArticuloPedido(models.Model):
+    orden = models.ForeignKey(Orden, on_delete=models.CASCADE, related_name = 'articulos')
+    producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
+    cantidad = models.PositiveIntegerField(default = 1)
+
+class Cart(models.Model):
+    usuario = models.fore
